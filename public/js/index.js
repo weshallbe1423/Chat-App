df8241680f701ca76f47a095ba620bac6f8dbb61
weshallbe1423 <vishalnikalje23@gmail.com>
2021-03-03 17:50:20 +0530

    created message from user to server

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+let socket=io();
+socket.on('connect',function(){
+    console.log("Connected to the Server.");
+    socket.emit('createMessage',{
+        from:"Vishal",
+        text:"Hi good things comming towards you lets capture those",
+        createdAt:new Date().getTime()
+    })
+});
+
+socket.on('disconnect',function(){
+    console.log("Disconnected from the Server.");
+});
